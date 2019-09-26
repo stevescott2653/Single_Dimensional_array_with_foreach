7ef13fa2dbd4eb876fcfdf381545a6754952a6d5
Steven Scott <stevescott2653@gmail.com>
2019-09-26 12:06:29 -0400

    Add files via upload

---
+++ Single Dimensional array with foreach/Single Dimensional array with foreach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Dimensional_array_with_foreach
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50 }; //creating and initializing array
+            
+            //Traversing Array
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
